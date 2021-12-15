af92ed72eaf8b640ce424b83c879341476a27940
Joathan Souza <joathanf@gmail.com>
2021-12-15 15:25:32 -0300

    refatorando o seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-900.times.each {|s| UrlShortener.shorten("http://meusite#{s}.com.br")}
+puts 'Criando base...'
+150.times.each { |s| UrlShortener.shorten("http://meusite#{s}.com.br") }
 
 ShortUrl.all.each do |url|
   url.update(hits: rand(999))
 end
+puts '[Ok] Base foi criada...'
